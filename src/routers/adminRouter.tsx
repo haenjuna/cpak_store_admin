01880fe681f6411cac73f37826e15a36dff320fd
MinJae Kim <skwze@naver.com>
2024-10-02 14:44:22 +0900

    .

--- src/routers/adminRouter.tsx
+++ src/routers/adminRouter.tsx
@@ -1,4 +1,4 @@
-import {createBrowserRouter} from "react-router-dom";
+import {createBrowserRouter, Navigate} from "react-router-dom";
 import {lazy, Suspense} from "react";
 
 import LoadingPage from "../pages/LoadingPage.tsx";
@@ -10,6 +10,7 @@ const AdminMainPage = lazy(() => import("../pages/AdminMainPage.tsx"))
 export const Loading = <LoadingPage></LoadingPage>
 
 const adminRouter = createBrowserRouter([
+    {path: "", element: <Navigate to='login' replace={true}></Navigate>},
     {
         path: "/",
         element: <Suspense fallback={Loading}><AdminMainPage/></Suspense> ,
