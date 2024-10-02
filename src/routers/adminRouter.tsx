02b6f743f2e1265d5fd9808a4220afe4f7905438
Hyeonjun Park <baghyeonjun16@gmail.com>
2024-10-02 15:20:20 +0900

    pull전 test커밋

--- src/routers/adminRouter.tsx
+++ src/routers/adminRouter.tsx
@@ -1,4 +1,4 @@
-import {createBrowserRouter} from "react-router-dom";
+import {createBrowserRouter, Navigate} from "react-router-dom";
 import {lazy, Suspense} from "react";
 
 import LoadingPage from "../pages/LoadingPage.tsx";
