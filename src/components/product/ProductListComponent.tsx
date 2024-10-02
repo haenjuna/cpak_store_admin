6ad7c5cbf524715521cf4c45259b25b77c8d14e3
AHN <gd20624@gmail.com>
2024-10-02 14:08:53 +0900

    2024-10-02 product.ts ProductAPI.ts ProductListComponent.tsx ListPage 수정

--- src/components/product/ProductListComponent.tsx
+++ src/components/product/ProductListComponent.tsx
@@ -1,7 +1,6 @@
 import {getProductList} from "../../apis/ProductAPI.ts";
 import {useEffect, useState} from "react";
 import {IPageResponse, IProduct} from "../../types/product.ts";
-import {useNavigate} from "react-router";
 import PageComponent from "../common/PageComponent.tsx";
 import {useSearchParams} from "react-router-dom";
 
@@ -26,7 +25,6 @@ function ProductListComponent() {
     const page: number = Number(query.get("page")) || 1
     const size: number = Number(query.get("size")) || 10
 
-
     useEffect(() => {
         getProductList(page,size).then(data => {
             setPageResponse(data)
@@ -34,18 +32,10 @@ function ProductListComponent() {
     }, [page,size]);
 
 
-    const navigate = useNavigate();
 
     const ListLi = pageResponse.dtoList.map((product:IProduct)=>{
 
         const {pno, pname, pdesc, price, uploadFileNames} = product
-
-        const moveToRead = (pno: number | undefined) => {
-            navigate({
-                pathname: `/product/read/:${pno}`,
-            })
-        }
-
 
         return (
 
@@ -74,21 +64,10 @@ function ProductListComponent() {
                 </td>
                 <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm dark:bg-gray-800 dark:border-gray-700">
                     <div className="flex items-center space-x-4">
-                        <button onClick={() => moveToRead(pno)}
-                                className="text-purple-600 hover:text-purple-900 dark:text-purple-400 dark:hover:text-purple-300">
-                            <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
-                                <path
-                                    d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z"></path>
-                            </svg>
+                        <button className="text-purple-600 hover:text-purple-900 dark:text-purple-400 dark:hover:text-purple-300 border-purple-50">
+                            조회
                         </button>
-                        <button onClick={() => moveToRead(pno)}
-                                className="text-red-600 hover:text-red-900 dark:text-red-400 dark:hover:text-red-300">
-                            <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
-                                <path fillRule="evenodd"
-                                      d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
-                                      clipRule="evenodd"></path>
-                            </svg>
-                        </button>
+
                     </div>
                 </td>
             </tr>
