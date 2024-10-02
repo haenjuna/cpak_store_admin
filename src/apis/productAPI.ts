4751a09ff59c88b09970a8ac3d0e74698dc3e976
MinJae Kim <skwze@naver.com>
2024-10-02 15:54:45 +0900

    CRUD 작업 1차 통합 테스트_KMJ ( 2024.10.02 ) 타입 수정

--- src/apis/productAPI.ts
+++ src/apis/productAPI.ts
@@ -1,13 +1,8 @@
 import Axios from "axios";
-import {IPageResponse, IProduct} from "../types/product.ts";
+import {IPageResponse} from "../types/product.ts";
 import axios from "axios";
 
 const host:string ='http://118.38.219.107:8089/api/products';
-
-const header =
-    {
-        headers: {'content-type' : 'multipart/form-data'}
-    }
 
 export const getProductList = async( page?:number, size?:number): Promise<IPageResponse> => {
 
@@ -25,28 +20,4 @@ export const getOne = async(pno:number) : Promise<IPageResponse> => {
     const res = await axios.get(`${host}/${pno}`)
 
     return res.data;
-}
-
-export const postAdd = async (formData: FormData): Promise<number> => {
-
-    const res = await axios.post(`${host}/`, formData, header)
-
-    console.log(res)
-    // return Number(res.data)
-    return 0
-}
-
-
-export const putOne = async  (product: IProduct, pno: number): Promise<IProduct> => {
-
-    const res = await axios.put(`${host}/${pno}`, product)
-
-    return res.data
-}
-
-export const deleteOne = async  (pno: number): Promise<IProduct> => {
-
-    const res = await axios.delete(`${host}/${pno}`)
-
-    return res.data
 }
